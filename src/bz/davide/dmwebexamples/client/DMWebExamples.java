524b3cb82b3d866454c46bac6ba485b52bf5237d
davide <d@vide.bz>
2013-11-07 20:41:16 +0100

    setup empty gwt project

---
+++ src/bz/davide/dmwebexamples/client/DMWebExamples.java
@@ -0,0 +1,36 @@
+/*
+DMWebExamples - Examples for the Java DMWeb framework - http://www.davide.bz/dmweb
+
+Copyright (C) 2013 Davide Montesin <d@vide.bz> - Bolzano/Bozen - Italy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+package bz.davide.dmwebexamples.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class DMWebExamples implements EntryPoint
+{
+   /**
+    * This is the entry point method.
+    */
+   @Override
+   public void onModuleLoad()
+   {
+   }
+}
